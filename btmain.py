cf2c674102b3d1abb681f2b0a09b9a7ae347f2be
M_K_J <kevin911012@gmail.com>
2022-10-20 14:31:18 +0800

    將結果輸出到excel檔

--- btmain.py
+++ btmain.py
@@ -10,21 +10,14 @@ import requests
 from indicator import *
 from IPython.core.interactiveshell import InteractiveShell
 InteractiveShell.ast_node_interactivity = "all"
-
-
+plt.switch_backend('agg')
 # 下載10年間的股票數據
-_end_ = dt.date.today()
-year = dt.datetime.now().year
-_start_ = dt.date(year - 10,1,2)
-ticker = input("The tick you want to backtest: ")
-df = yf.download(ticker, start = _start_, end = _end_)
-
-# 計算單日漲跌幅
-df['Rate of Price Spread'] = Rate_of_Price_Spread(df)
-
-# 計算RSI
-df['RSI'] = RSI(df, 14)
-
-
+#symbols = input("The tick you want to backtest: ")
+symbols = 'TSLA'    
+ticker = yf.Ticker(symbols)
+df = ticker.history(period='10y', interval='1d')
+Aroon(df, 14)
 print(*df)
 print(df.tail(5).sort_index(ascending = False))
+
+df.to_excel(R'Result.xlsx', sheet_name='Result')
